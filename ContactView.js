96a152baea072200b6cf93adff3fb43d6c92a02b
bonnichiwa <bonnie.pham@ryerson.ca>
2017-11-12 23:22:31 +0400

    entry point now has values updated passed from form to view to main

--- ContactView.js
+++ ContactView.js
@@ -2,12 +2,12 @@ var ContactView = React.createClass({
 	propTypes: {
 		contacts: React.PropTypes.array.isRequired,
 		newContact: React.PropTypes.object.isRequired,
+		onNewContactChange: React.PropTypes.func.isRequired,
 	},
 
 	render: function() {
 		var contactItemElements = this.props.contacts
-		.map(function(contact) { return React.createElement(ContactItem, contact)
-		})
+		.map(function(contact) { return React.createElement(ContactItem, contact) })
 
 		return (
 			React.createElement('div', {className: 'ContactView'},
@@ -15,7 +15,7 @@ var ContactView = React.createClass({
 				React.createElement('ul', {className: 'ContactView-list'}, contactItemElements),
 				React.createElement(ContactForm, {
 					value: this.props.newContact,
-					onChange: function(contact) { console.log(contact) },
+					onChange: this.props.onNewContactChange
 				})
 			)
 		)
--- main.js
+++ main.js
@@ -1,21 +1,35 @@
 /**
- * Dummy data
+ * Actions
  */
-var contacts = [
-	{key: 1, name: "Bonnie Pham", email: "dear.bonnie.pham@gmail.com", description: "Corgi fanatic"},
-	{key: 2, name: "Christine Pham", email: "pham.christine90@gmail.com", description: "Corgi fanatic #2"},
-	{key: 3, name: "James Leung", email: "james.leung83@gmail.com", description: "Corgi fanatic #2's follower"},
-]
-
-var newContact = {name: "", email: "", description: ""}
+function updateNewContact(contact) {
+	setState({ newContact: contact })
+}
 
 /**
- * Entry point
+ * State
  */
-ReactDOM.render(
-	React.createElement(ContactView, {
-		contacts: contacts,
-		newContact: newContact
-	}),
-	document.getElementById('react-app')
-)
+
+// The app's complete current states
+var state = {};
+
+// Make the given changes to the state and perform any required housekeeping
+function setState(changes) {
+	Object.assign(state, changes);
+
+	ReactDOM.render(
+		React.createElement(ContactView, Object.assign({}, state, {
+			onNewContactChange: updateNewContact,
+		})),
+		document.getElementById('react-app')
+	)
+}
+
+// Set inital state
+setState({
+	contacts: [
+		{key: 1, name: "Bonnie Pham", email: "dear.bonnie.pham@gmail.com", description: "Corgi fanatic"},
+		{key: 2, name: "Christine Pham", email: "pham.christine90@gmail.com", description: "Corgi fanatic #2"},
+		{key: 3, name: "James Leung", email: "james.leung83@gmail.com", description: "Corgi fanatic #2's follower"},
+	],	
+	newContact: {name: "", email: "", description: ""}
+})
